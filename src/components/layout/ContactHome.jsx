c89f494872bf54a71c431a4ba5a1cada40c0ff65
ishanaudichya <93155612+ishanaudichya@users.noreply.github.com>
2024-05-27 09:57:56 +0530

    Update ContactHome.jsx

--- src/components/layout/ContactHome.jsx
+++ src/components/layout/ContactHome.jsx
@@ -1,12 +1,16 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
 function ContactHome() {
+  const navigate = useNavigate();
   return (
     <div>
-      <header className="contact-hero">
-        <div className="contact-content">
-          <div className="contact-text"></div>
-        </div>
+      <header
+        onClick={() => {
+          navigate("/contact");
+        }}
+        className="contact-hero"
+      >
+        <div className="contact-content"></div>
       </header>
     </div>
   );
