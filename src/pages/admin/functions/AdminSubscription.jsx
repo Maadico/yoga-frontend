45481e59fe5364c82efaf4a27a4996c9377b4df1
ishanaudichya <93155612+ishanaudichya@users.noreply.github.com>
2024-06-05 04:21:36 +0530

    amount

--- src/pages/admin/functions/AdminSubscription.jsx
+++ src/pages/admin/functions/AdminSubscription.jsx
@@ -30,6 +30,7 @@ function AdminSubscription() {
             <th>Name</th>
             <th>Email</th>
             <th>Merchant Transaction ID</th>
+            <th>Amount</th>
             <th>Course Name</th>
             <th>Active</th>
             <th>Days Left</th>
@@ -41,6 +42,7 @@ function AdminSubscription() {
               <td>{subscription.userName}</td>
               <td>{subscription.userEmail}</td>
               <td>{subscription.merchantTransactionId}</td>
+              <td>{subscription.amount}</td>
               <td>{subscription.courseName}</td>
               <td>{subscription.isActive ? "Active" : "Expired"}</td>
               <td>{subscription.daysLeft}</td>
